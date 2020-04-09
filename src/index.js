58ec1d8ef2520429357cbc0563c003e0cedc3547
judelfino <juscie@gmail.com>
2020-04-09 08:21:03 -0500

    chore: Commit inicial - aula nínvel-1

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/projects', (request, response) => {
+  return response.json({ message: 'Oi Hello World' });
+});
+
+app.post('/projects', (request, response) => {
+  return response.json([
+    'projeto 1',
+    'projeto 2',
+    'projeto 3',
+  ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+  return response.json([
+    'projeto 4',
+    'projeto 2',
+    'projeto 3',
+  ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json([
+    'projeto 2',
+    'projeto 3',
+  ]);
+});
+
+app.listen(3333, () => {
+  console.log('🚀Back-end started');
+});
